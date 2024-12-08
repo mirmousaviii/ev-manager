d38d2d5b8f00c0eb78926cd688cb70a088e0d340
Mostafa Mirmousavi <mirmousavi.m@gmail.com>
2024-12-08 13:42:26 +0100

    chore: setup favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,13 @@ import type { Metadata } from "next";
 import "./styles/globals.css";
 import React from "react";
 
+
 export const metadata: Metadata = {
   title: "EV Charging",
   description: "A simple EV charging simulation",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
